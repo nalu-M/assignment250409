0edbce092f849afaa747451ece4f100618116819
Matsuura <asxkb42597@gmail.com>
2025-04-09 14:53:02 +0900

    commit2504091452

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,8 +10,10 @@ export type AmplifyDependentResourcesAttributes = {
     "assignment25046b2b1368": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
